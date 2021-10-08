e738429b0db2d0c469efde4b0bc5466b22c05abf
scroll <shrookashraf24@gmail.com>
2021-10-08 04:30:37 +0200

    part1

---
+++ Embedded_C/Assignment3_unit3_lec3/part1/Platform_types.h
@@ -0,0 +1,32 @@
+/*
+ * Platform_types.h
+ *
+ *  Created on: Sep 29, 2021
+ *      Author: DELL
+ */
+
+#ifndef PLATFORM_TYPES_H_
+#define PLATFORM_TYPES_H_
+
+typedef volatile unsigned int vuint32_t;
+typedef volatile int vint32_t;
+typedef unsigned int uint32_t;
+typedef int int32_t;
+
+typedef unsigned char uint8_t;
+typedef char int8_t;
+typedef volatile unsigned char vuint8_t;
+typedef volatile char vint8_t;
+
+typedef unsigned short uint16_t;
+typedef short int16_t;
+typedef volatile unsigned short vuint16_t;
+typedef volatile short vint16_t;
+
+typedef unsigned long long uint64_t;
+typedef long long int64_t;
+typedef volatile unsigned long long vuint64_t;
+typedef volatile long long vint64_t;
+
+
+#endif /* PLATFORM_TYPES_H_ */
